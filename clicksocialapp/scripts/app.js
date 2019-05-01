f22c7d6f9452f1b7dd133c967614d1df96824e04
thienlm <thienlm@onetech.vn>
2019-05-01 10:10:48 +0700

    first commit

--- clicksocialapp/scripts/app.js
+++
@@ -1,2 +0,0 @@
-var App=function(){return{init:function(){},getViewPort:function(){var n=window,t="inner";return"innerWidth"in window||(t="client",n=document.documentElement||document.body),{width:n[t+"Width"],height:n[t+"Height"]}},getResponsiveBreakpoint:function(n){var t={xs:480,sm:768,md:992,lg:1200};return t[n]?t[n]:0}}}();$(document).ready(function(){App.init()});
-//# sourceMappingURL=app.js.map
